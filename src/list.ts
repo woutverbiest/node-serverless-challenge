bdd7d7bcb686f8f6a65e9193d6c76cefa33cc279
wout.verbiest <wout.verbiest@student.howest.be>
2021-11-30 16:35:40 +0100

    list users

--- src/list.ts
+++ src/list.ts
@@ -1,5 +1,29 @@
-// Your project code
+import User from './Models/user';
 
-exports.handler = () => {
-  // Your code here
+exports.handler = async (event, context, callback) => {
+  const queryParameters = event.queryStringParameters;
+
+  const result = await listUsers(queryParameters);
+
+  callback(null, JSON.stringify(result));
 };
+
+/**
+ * fetches a list of users from database
+ *
+ * @param queryParameters all url query parameters
+ * @returns a list of users
+ * @returns error in case of a database error
+ */
+
+async function listUsers(queryParameters: any) {
+  try {
+    const users = await User.findAll({ where: queryParameters });
+    return users;
+  } catch (error: any) {
+    return {
+      statusCode: 500,
+      body: { error: error.name },
+    };
+  }
+}
