b6ba1fbc029cc039fd29815d333ef77143235674
wout.verbiest <wout.verbiest@student.howest.be>
2021-11-30 16:47:04 +0100

    add get user

--- src/get.ts
+++ src/get.ts
@@ -1,5 +1,29 @@
-// Your project code
+import User from './Models/user';
 
-exports.handler = () => {
-  // Your code here
+exports.handler = async (event, context, callback) => {
+  const id = event.pathParameters.id ?? null;
+
+  const result = await getUser(id);
+
+  callback(null, JSON.stringify(result));
 };
+
+/**
+ * returns a user by its row id
+ *
+ * @param id {number} id of wanted user
+ * @returns user
+ * @returns error in case of a database error
+ */
+
+async function getUser(id: number) {
+  try {
+    const user = await User.findByPk(id);
+    return user;
+  } catch (error: any) {
+    return {
+      statusCode: 500,
+      body: { error: error.name },
+    };
+  }
+}
