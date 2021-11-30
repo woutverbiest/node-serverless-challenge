900fb37700c357b585903fdf3d0302cfcab1d358
wout.verbiest <wout.verbiest@student.howest.be>
2021-11-30 17:24:40 +0100

    create new user

--- src/create.ts
+++ src/create.ts
@@ -1,5 +1,48 @@
-// Your project code
+import type { ValidationErrorItem } from 'sequelize/dist/lib/errors';
+import User from './Models/user';
 
-exports.handler = () => {
-  // Your code here
+exports.handler = async (event, context, callback) => {
+  const data = event.body;
+
+  const result = await createUser(data);
+
+  callback(null, JSON.stringify(result));
 };
+
+/**
+ * creates a new user
+ *
+ * @param data body from request
+ * @returns user
+ * @returns error in case of database error
+ * @returns error in case of validation error
+ */
+
+async function createUser(data: any) {
+  try {
+    const user = await User.create(JSON.parse(data));
+    return user;
+  } catch (error: any) {
+    console.log(error);
+
+    if (error.errors) {
+      const validationErrors =
+        error.errors.map((err: ValidationErrorItem) => err.message) ?? [];
+
+      return {
+        statusCode: 400,
+        body: {
+          error: error.name,
+          validationErrors: validationErrors,
+        },
+      };
+    }
+
+    return {
+      statusCode: 500,
+      body: {
+        error: error.name,
+      },
+    };
+  }
+}
